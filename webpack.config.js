7522842bcce0d4eb6b01501d1174c516a53efcfb
Flo <florian.unternaehrer@simplificator.com>
2018-08-03 14:47:15 +0200

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -16,12 +16,12 @@
 
 'use strict';
 
-const CssBundleFactory = require('../scripts/webpack/css-bundle-factory');
-const Environment = require('../scripts/build/environment');
-const Globber = require('../scripts/webpack/globber');
-const JsBundleFactory = require('../scripts/webpack/js-bundle-factory');
-const PathResolver = require('../scripts/build/path-resolver');
-const PluginFactory = require('../scripts/webpack/plugin-factory');
+const CssBundleFactory = require('./scripts/webpack/css-bundle-factory');
+const Environment = require('./scripts/build/environment');
+const Globber = require('./scripts/webpack/globber');
+const JsBundleFactory = require('./scripts/webpack/js-bundle-factory');
+const PathResolver = require('./scripts/build/path-resolver');
+const PluginFactory = require('./scripts/webpack/plugin-factory');
 
 const env = new Environment();
 env.setBabelEnv();
@@ -29,66 +29,16 @@ env.setBabelEnv();
 const pathResolver = new PathResolver();
 const globber = new Globber({pathResolver});
 const pluginFactory = new PluginFactory({globber});
-const copyrightBannerPlugin = pluginFactory.createCopyrightBannerPlugin();
 const cssBundleFactory = new CssBundleFactory({env, pathResolver, globber, pluginFactory});
 const jsBundleFactory = new JsBundleFactory({env, pathResolver, globber, pluginFactory});
 
-const DEMO_BASE_DIR_ABSOLUTE_PATH = pathResolver.getAbsolutePath('/demos');
-
 const OUTPUT = {
-  fsDirAbsolutePath: pathResolver.getAbsolutePath('/build'),
-  httpDirAbsolutePath: '/assets/',
+  fsDirAbsolutePath: pathResolver.getAbsolutePath('./build'),
 };
 
 module.exports = [
-  mainJsCombined(),
-  demoCss(),
-  demoJs(),
+  jsBundleFactory.createMainJsCombined({output: OUTPUT}),
+  jsBundleFactory.createMainJsALaCarte({output: OUTPUT}),
+  cssBundleFactory.createMainCssCombined({output: OUTPUT}),
+  cssBundleFactory.createMainCssALaCarte({output: OUTPUT}),
 ];
-
-// webpack-dev-server requires that these properties be set on the first bundle.
-// It ignores them on all other bundles.
-Object.assign(module.exports[0], {
-  devServer: {
-    contentBase: DEMO_BASE_DIR_ABSOLUTE_PATH,
-
-    // See https://github.com/webpack/webpack-dev-server/issues/882
-    // Because we only spin up dev servers temporarily, and all of our assets are publicly
-    // available on GitHub, we can safely disable this check.
-    disableHostCheck: true,
-  },
-});
-
-function mainJsCombined() {
-  return jsBundleFactory.createMainJsCombined({output: OUTPUT});
-}
-
-function demoJs() {
-  return jsBundleFactory.createCustomJs({
-    bundleName: 'demo-js',
-    chunks: {
-      'common': pathResolver.getAbsolutePath('./demos/common.js'),
-      'theme/index': pathResolver.getAbsolutePath('./demos/theme/index.js'),
-    },
-    output: Object.assign({}, OUTPUT, {
-      filename: '[name].js',
-      library: ['demo', '[name]'],
-    }),
-    plugins: [
-      copyrightBannerPlugin,
-    ],
-  });
-}
-
-function demoCss() {
-  return cssBundleFactory.createCustomCss({
-    bundleName: 'demo-css',
-    chunkGlobConfig: {
-      inputDirectory: '/demos',
-    },
-    output: OUTPUT,
-    plugins: [
-      copyrightBannerPlugin,
-    ],
-  });
-}
